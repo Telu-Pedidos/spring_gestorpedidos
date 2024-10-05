84ec0bde4b1f467a8c3692bff233a8e71a09f649
jorgeguedess <jorge_guedes00@hotmail.com>
2024-10-04 23:53:37 -0300

    fix: adjusting generateSlug

--- src/main/java/com/api/gestor_pedidos_telu/utils/Slug.java
+++ src/main/java/com/api/gestor_pedidos_telu/utils/Slug.java
@@ -4,12 +4,20 @@ import java.text.Normalizer;
 
 public class Slug {
     public static String generateSlug(String name) {
-        if (name != null && !name.trim().isEmpty()) {
-            String normalized = Normalizer.normalize(name.trim(), Normalizer.Form.NFD);
-            String slug = normalized.replaceAll("\\p{M}", "");
-
-            return slug.toLowerCase().replaceAll(" ", "-").replaceAll("-+", "-");
+        if (name == null || name.trim().isEmpty()) {
+            return "";
         }
-        return "";
+
+        // Normaliza o texto, remove acentos e barras
+        String normalized = Normalizer.normalize(name.trim(), Normalizer.Form.NFD)
+                .replaceAll("\\p{M}", "") // Remove caracteres de acentuação
+                .replaceAll("[/]", ""); // Remove barras
+
+        // Converte para minúsculas, substitui espaços por hífens e remove hífens duplicados
+        String slug = normalized.toLowerCase()
+                .replaceAll(" ", "-")
+                .replaceAll("-+", "-");
+
+        return slug;
     }
 }
